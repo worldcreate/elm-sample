9cf716d674d73f6a2d2fa1453cf7f34e4dd9f7ec
matsumoto <r-matsumoto@zyyx.jp>
2019-09-26 12:44:54 +0900

    TODOを追加できるように変更

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,7 @@
 import Browser
-import Html exposing (Html, button, div, text, h1, input)
-import Html.Events exposing (onInput)
+import Html exposing (Html, button, div, text, h1, input, ul, li)
+import Html.Events exposing (onInput, onClick)
+import Html.Attributes exposing (value)
 
 main = 
     Browser.sandbox {
@@ -12,26 +13,42 @@ main =
 -- MODEL
 
 type alias Model = {
-    input: String
+    input: String,
+    todos: List String
     }
 
 init : Model
 init = {
-    input = ""
+    input = "",
+    todos = []
     }
 
 -- UPDATE
 
-type Msg = Input String
+type Msg = 
+    Input String
+    | Save
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         Input newInput ->
             ({model | input = newInput })
+        Save ->
+            ({model | todos = List.concat [model.todos, [model.input]], input = ""})
 
 -- VIEW
 
 view: Model -> Html Msg
 view model =
-    div [] [h1 [] [text "TODO!!"], div [] [input [ onInput Input ] []], div [] [text model.input] ]
+    div [] [
+        h1 [] [text "TODO!!"],
+        div [] [
+            ul [] (List.map (\a -> li [] [text a]) model.todos)
+        ],
+        div [] [
+            input [ onInput Input, value model.input ] [],
+            div [] [button [ onClick Save ] [text "save"]]
+        ]
+    
+    ]
